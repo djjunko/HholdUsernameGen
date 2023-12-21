31098f3225a34914d93a200b963a62e57968cbb9
djjunko <92181429+djjunko@users.noreply.github.com>
2023-12-20 22:59:35 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById('splash-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 2000);
+};
